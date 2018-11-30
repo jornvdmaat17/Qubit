7ae43ddbc5f201644245c329462468ad4bacf110
0976779@hr.nl <0976779@hr.nl>
2018-11-30 14:10:53 +0100

    Added getRandomQubit and also made some examples

--- Qubit.py
+++ Qubit.py
@@ -4,17 +4,17 @@ from math import sqrt,e
 from random import randint
 import QuantumConstants
 
-offset = 0.000001
+OFFSET = 0.000001
 
 class Qubit:
+
     def __init__(self, a, b):
         self.__read = False
-        if 1 - offset < abs(a * a) + abs(b * b) < 1 + offset:
+        if 1 - OFFSET < abs(a * a) + abs(b * b) < 1 + OFFSET:
             self.__a = a
             self.__b = b
             self.__matrix = np.matrix([[self.__a],[self.__b]])
         else:
-            print(a)
             raise impossibleQubitException("Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b)), None)
 
     def read(self):
@@ -32,50 +32,61 @@ class Qubit:
     def pauliX(self):
         if not self.__read:
             tmp = QuantumConstants.PAULIX * self.__matrix
-            return Qubit(tmp.item(0), tmp.item(1))
-        else:
-            return self
+            self.__a = tmp.item(0)
+            self.__b = tmp.item(1)
 
     def pauliY(self):
         if not self.__read:
             tmp = QuantumConstants.PAULIY * self.__matrix
-            return Qubit(tmp.item(0), tmp.item(1))
-        else:
-            return self
+            self.__a = tmp.item(0)
+            self.__b = tmp.item(1)
 
     def pauliZ(self):
         if not self.__read:
             tmp = QuantumConstants.PAULIZ * self.__matrix
-            return Qubit(tmp.item(0), tmp.item(1))
-        else:
-            return self
+            self.__a = tmp.item(0)
+            self.__b = tmp.item(1)
 
     def hadamard(self):
         if not self.__read:
             tmp = QuantumConstants.HADAMARD * self.__matrix
-            return Qubit(tmp.item(0), tmp.item(1))
-        else:
-            return self
+            self.__a = tmp.item(0)
+            self.__b = tmp.item(1)
 
     def sqrtNot(self):
         if not self.__read:
             tmp = QuantumConstants.SQRTNOT * self.__matrix
-            return Qubit(tmp.item(0), tmp.item(1))
-        else:
-            return self
+            self.__a = tmp.item(0)
+            self.__b = tmp.item(1)
 
     def rphi(self):
         if not self.__read:
             tmp = QuantumConstants.RPHI * self.__matrix
-            return Qubit(tmp.item(0), tmp.item(1))
-        else:
-            return self
+            self.__a = tmp.item(0)
+            self.__b = tmp.item(1)
 
     def __str__(self):
         return str(self.__a) + "|0> + " + str(self.__b) + "|1>"
+
 
 class impossibleQubitException(Exception):
     def __init__(self, message, errors):
         
         super().__init__(message)
         self.__errors = errors
+
+
+def getRandomQubit(negPossible, irPossinble):
+    randAlpha = randint(0, 100) / 100
+    randBeta = 1.0 - randAlpha
+    randAlpha = sqrt(randAlpha)
+    randBeta = sqrt(randBeta)
+    if randint(0,1) == 1 and negPossible:
+        randAlpha *= -1
+    if randint(0,1) == 1 and negPossible:
+        randBeta *= -1
+    if randint(0,1) == 1 and irPossinble:
+        randAlpha *= 1j
+    if randint(0,1) and irPossinble:
+        randBeta *= 1j    
+    return Qubit(randAlpha, randBeta)
