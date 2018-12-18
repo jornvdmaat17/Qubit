5645fdef2bbb62c538a34960971f5e6fa8883b30
0976779@hr.nl <0976779@hr.nl>
2018-12-18 14:11:37 +0100

    First steps to visual part

---
+++ GekkeHTTPServer.py
@@ -0,0 +1,102 @@
+from flask import Flask,request
+from math import sqrt,e
+import numpy as np
+
+# Math constants
+GOLDEN = (1 + 5 ** 0.5) / 2 #This value is about: 1.618033988749895
+HS2 = 1 / sqrt(2)   #This value is about: 0.7071067811865475
+
+# Matrices to be used with quantum calculations
+IDENTITY = np.matrix([[1, 0],[0,1]])
+PAULIX = np.matrix([[0,1],[1,0]])
+PAULIY = np.matrix([[0,1j],[1j,0]])
+PAULIZ = np.matrix([[1,0],[0,-1]])
+HADAMARD = HS2 * np.matrix([[1, 1], [1, -1]])
+SQRTNOT = (1/2) * np.matrix([[1+1j, 1 - 1j], [1 - 1j, 1+1j]])
+# RPHI = np.matrix([[1,0],[0,e ** (1j * GOLDEN)]])
+OFFSET = 0.000001
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome to my Flask page!"
+
+@app.route("/get/pauliX/<Alpha>:<Beta>", methods=['GET'])
+def get_pauliX(Alpha,Beta):
+    Alpha = complex(Alpha)
+    Beta = complex(Beta)
+    if 1 - OFFSET < abs(Alpha * Alpha) + abs(Beta * Beta) < 1 + OFFSET:
+        tmp = PAULIX * np.matrix([[Alpha],[Beta]])
+        a = tmp.item(0)
+        b = tmp.item(1)
+
+        return str(a) + ":" + str(b)
+    else: return "impossibleQubitException: Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b))
+
+@app.route("/get/pauliY/<Alpha>:<Beta>", methods=['GET'])
+def get_pauliY(Alpha,Beta):
+    Alpha = complex(Alpha)
+    Beta = complex(Beta)
+    if 1 - OFFSET < abs(Alpha * Alpha) + abs(Beta * Beta) < 1 + OFFSET:
+        tmp = PAULIY * np.matrix([[Alpha],[Beta]])
+        a = tmp.item(0)
+        b = tmp.item(1)
+
+        return str(a) + ":" + str(b)
+    else: return "impossibleQubitException: Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b))
+
+@app.route("/get/pauliZ/<Alpha>:<Beta>", methods=['GET'])
+def get_pauliZ(Alpha,Beta):
+    Alpha = complex(Alpha)
+    Beta = complex(Beta)
+    if 1 - OFFSET < abs(Alpha * Alpha) + abs(Beta * Beta) < 1 + OFFSET:
+        tmp = PAULIZ * np.matrix([[Alpha],[Beta]])
+        a = tmp.item(0)
+        b = tmp.item(1)
+
+        return str(a) + ":" + str(b)
+    else: return "impossibleQubitException: Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b))
+
+@app.route("/get/hadamard/<Alpha>:<Beta>", methods=['GET'])
+def get_hadamard(Alpha,Beta):
+    Alpha = complex(Alpha)
+    Beta = complex(Beta)
+    if 1 - OFFSET < abs(Alpha * Alpha) + abs(Beta * Beta) < 1 + OFFSET:
+        tmp = HADAMARD * np.matrix([[Alpha],[Beta]])
+        a = tmp.item(0)
+        b = tmp.item(1)
+
+        return str(a) + ":" + str(b)
+    else: return "impossibleQubitException: Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b))
+
+@app.route("/get/sqrtNOT/<Alpha>:<Beta>", methods=['GET'])
+def get_sqrtNOT(Alpha,Beta):
+    Alpha = complex(Alpha)
+    Beta = complex(Beta)
+    if 1 - OFFSET < abs(Alpha * Alpha) + abs(Beta * Beta) < 1 + OFFSET:
+        tmp = SQRTNOT * np.matrix([[Alpha],[Beta]])
+        a = tmp.item(0)
+        b = tmp.item(1)
+
+        return str(a) + ":" + str(b)
+    else: return "impossibleQubitException: Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b))
+
+@app.route("/get/rphi/<Alpha>:<Beta>:<phi>", methods=['GET'])
+def get_rphi(Alpha,Beta,phi):
+    Alpha = complex(Alpha)
+    Beta = complex(Beta)
+    if 1 - OFFSET < abs(Alpha * Alpha) + abs(Beta * Beta) < 1 + OFFSET:
+        tmp = np.matrix([[1,0],[0,e ** (1j * phi)]]) * np.matrix([[Alpha],[Beta]])
+        a = tmp.item(0)
+        b = tmp.item(1)
+
+        return str(a) + ":" + str(b)
+    else: return "impossibleQubitException: Expected A^2 + B^2 = 1, but was " + str(abs(a * a) + abs(b * b))
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=7234)
+
+#test
+
+
--- assets/js/MultiQubit.js
+++ assets/js/MultiQubit.js
@@ -1,33 +1,234 @@
+// Constants
 let startoffset = 100;
 let space = 20;
 let size = 60;
 let lspace = 10;
+let strokeOffset = 2;
+let sphereRadius = 10;
+let duplicateOffset = 5;
 
-var pXI;
+// Global
+var paulix;
+var pauliy;
+var pauliz;
+var onScreen = [];
+var startScreen = [];
+var dragging;
+var newScreenObject;
+var isLine = false;
+var startedLineObjIndex;
+var startedLineDotSide;
+
+function preload(){
+    paulix = loadImage('assets/img/paulix.png');
+    pauliy = loadImage('assets/img/pauliy.png');
+    pauliz = loadImage('assets/img/pauliz.png')
+}
 
 function setup(){
     createCanvas(600, 600);
+    var startPaulix = new ImageRect(0 + space, 0, paulix, "paulix");
+    var startPauliy = new ImageRect(0 + space, 60 + space, pauliy, "pauliy");
+    var startPauliz = new ImageRect(0 + space, 120 + space, pauliz, "pauliz");
+    startScreen.push(startPaulix);
+    startScreen.push(startPauliy);
+    startScreen.push(startPauliz);    
+}
+
+function draw(){ 
+    // if(newScreenObject != null){
+    //     clear();
+    // } 
     
-}
+    clear();
 
-function draw(){
-    /*
-    Menu for choosing
-    */
-
+    for(i=0;i<startScreen.length;i++){
+        startScreen[i].display();
+    }
+    stroke(strokeOffset);
     line(startoffset, 0, startoffset, 600);
-    var testC = new pauliX(50,50);
-    testC.display();
+
+    for(i=0;i<onScreen.length;i++){
+        onScreen[i].display();
+    }
+    if(newScreenObject != null && !isLine){
+        newScreenObject.setPos(mouseX - size/2, mouseY - size/2);
+        newScreenObject.display();
+    }
+    if(newScreenObject != null && isLine){
+        newScreenObject.setPos(mouseX, mouseY);
+        newScreenObject.display();
+    }
+
+    onScreen.push(new inputQubit(200, 200));
 }
 
-function pauliX(x, y){
+function mousePressed(){
+    for(i=0;i<startScreen.length;i++){
+        if(startScreen[i].inRange()){
+            newScreenObject = startScreen[i].duplicate();
+            break;
+        }
+    }
+    for(i=0;i<onScreen.length;i++){
+        if(onScreen[i].leftDotInRange() && !onScreen[i].leftConnected){
+            var tmp = onScreen[i].getLeftDot();
+            newScreenObject = new Line(tmp[0], tmp[1], mouseX, mouseY);
+            isLine = true;
+            startedLineObjIndex = i;
+            startedLineDotSide = "left";
+        }
+        if(onScreen[i].rightDotInRange() && !onScreen[i].rightConnected){
+            var tmp = onScreen[i].getRightDot();
+            newScreenObject = new Line(tmp[0], tmp[1], mouseX, mouseY);
+            isLine = true;
+            startedLineObjIndex = i;
+            startedLineDotSide = "right";
+        }
+    }
+}
+
+function mouseReleased(){
+    if(newScreenObject != null && !isLine){
+        onScreen.push(newScreenObject);    
+    }
+    if(newScreenObject != null && isLine){
+        for(i=0;i<onScreen.length;i++){
+            if(!startedLineObjIndex == i){
+                var found = false;
+                if(onScreen[i].leftDotInRange() && !onScreen[i].leftConnected){
+                    onScreen.push(newScreenObject);
+                    onScreen[i].setLeftConnected(true);
+                    found = true;
+                }
+                if(onScreen[i].rightDotInRange() && !onScreen[i].rightConnected){
+                    onScreen.push(newScreenObject);
+                    onScreen[i].setRightConnected(true);
+                    found = true;
+                }
+                if(found){
+                    if(startedLineDotSide == "left"){
+                        onScreen[startedLineObjIndex].setLeftConnected(true);
+                    }
+                    if(startedLineDotSide == "right"){
+                        onScreen[startedLineObjIndex].setRightConnected(true);
+                    }
+                    break;
+                }
+            }  
+        }
+        isLine = false;
+    }
+    startedLineObjIndex = null;
+    startedLineDotSide = null;
+    newScreenObject = null;
+}
+
+function ImageRect(x, y, img, name){
     this.x = x;
     this.y = y;
+    this.img = img;
+    this.name = name;
+    this.leftConnected = false;
+    this.rightConnected = false;
 
     this.display = function(){
-        rect(this.x, this.y, size, size);
-        line(this.x + lspace, this.y + lspace, this.x + size - lspace, this.y + size - lspace );
-        line(this.x + lspace, this.y + size + lspace, this.x + size + lspace, this.y - size + lspace);
-    };
+        image(img ,this.x, this.y, size, size); 
+        fill(0); 
+        ellipse(this.x + 1, this.y + 1 + size / 2, sphereRadius);  
+        ellipse(this.x - 1 + size, this.y + 1 + size / 2, sphereRadius);      
+    }
 
-};
+    this.inRange = function(){
+        if(mouseX >= this.x && mouseX <= this.x + size && mouseY >= this.y && mouseY<= this.y + size ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Todo
+    this.leftDotInRange = function(){
+        return (sq(mouseX - (this.x + 1)) + sq(mouseY - (this.y - 1 + size / 2))) < sphereRadius * sphereRadius;
+    }
+
+    this.rightDotInRange = function(){
+        return sq(mouseX - (this.x - 1 + size)) + sq(mouseY - (this.y + 1 + size / 2)) < sphereRadius * sphereRadius;
+    }
+
+    this.getLeftDot = function(){
+        return [this.x + 1, this.y - 1 + size / 2];
+    }
+
+    this.getRightDot = function(){
+        return [this.x - 1 + size, this.y + 1 + size / 2];
+    }
+
+    this.duplicate = function(){
+        return new ImageRect(this.x + 5, this.y + 5, img, name);
+    }
+
+    this.setPos = function(x , y){
+        this.x = x;
+        this.y = y;
+    }
+
+    this.setLeftConnected = function(left){
+        this.leftConnected = left;        
+    }
+
+    this.setRightConnected = function(right){
+        this.rightConnected = right;
+    }
+}
+
+function Line(x, y, dX, dY){
+    this.x = x;
+    this.y = y;
+    this.dX = dX;
+    this.dY = dY;
+
+    this.display = function(){
+        strokeWeight(3);
+        line(this.x, this.y, this.dX, this.dY);
+        strokeWeight(1);
+    }
+
+    this.setPos = function(dX, dY){
+        this.dX = dX;
+        this.dY = dY;
+    }
+
+    this.leftDotInRange = function(){
+        return false;
+    }
+
+    this.rightDotInRange = function(){
+        return false;
+    }
+}
+
+function inputQubit(x, y){
+    this.x = x;
+    this.y = y;
+    this.alpha = createInput().position(this.x, this.y);
+    this.beta = createInput().position(this.x, this.y + 23);
+
+    this.display = function(){
+        this.alpha.position(this.x, this.y);
+        this.beta.position(this.x,this.y + 23);
+    }
+
+    this.setPos = function(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    this.getAlphaValue = function(){
+        return this.alpha.value();
+    }
+
+    this.getBetaValue = function(){
+        return this.beta.value();
+    }
+}
